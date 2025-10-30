ee78526a87df9dde624a198a57b91fc783457b21
ITZsanthosh369 <tenyson452@gmail.com>
2025-10-30 16:43:09 +0530

    Update API base URL in Login, Register, and global context files

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState, useEffect } from "react";
 import axios from 'axios';
 
-const BASE_URL = "https://trackx-3mni4ufp.b4a.run/api/v1/";
+const BASE_URL = "https://trackx-qbs68ou4.b4a.run/api/v1/";
 
 const GlobalContext = React.createContext();
 
